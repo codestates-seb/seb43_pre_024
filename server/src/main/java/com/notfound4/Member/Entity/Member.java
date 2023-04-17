3dd675f32aa15aca320a9d6d3ae9c60d924750d4
mogu616 <jtk6747@gmail.com>
2023-04-17 15:42:39 +0900

    feat: Member entity 정의

--- server/src/main/java/com/notfound4/Member/Entity/Member.java
+++ server/src/main/java/com/notfound4/Member/Entity/Member.java
@@ -1,12 +1,63 @@
 package com.notfound4.Member.Entity;
 
-import javax.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
+@Getter
+@Setter
+@EntityListeners(AuditingEntityListener.class)
 public class Member {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long memberId;
 
+    @Column(length = 30, nullable = false)
+    private String name;
 
+    @Column(nullable = false, updatable = false, unique = true)
+    private String email;
 
+    @Column(length = 100, nullable = false)
+    private String password;
 
+    @Column(name = "profile_image", nullable = false)
+    private byte[] profileImage;
+
+    @Enumerated(value = EnumType.STRING)
+    @Column
+    private Status status = Status.ACTIVE;
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createAt;
+
+    @LastModifiedDate
+    @Column(name = "modified_at")
+    private LocalDateTime modifiedAt;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    private List<String> role = new ArrayList<>();
+
+    public enum Status {
+        ACTIVE("활동중"),
+        LOCK("휴면 상태");
+
+        @Getter
+        private String status;
+
+        Status(String status) {
+            this.status = status;
+        }
+
+    }
 }
