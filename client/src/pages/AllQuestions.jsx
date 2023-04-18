69868469bab28300d19f36ab9d67be88a9114b20
jieuny0314 <ccomo0313@naver.com>
2023-04-18 14:52:25 +0900

    Feat : json-server 실행 및 더미데이터 생성

--- client/src/pages/AllQuestions.jsx
+++ client/src/pages/AllQuestions.jsx
@@ -3,12 +3,38 @@ import styled from 'styled-components';
 const Box = styled.div`
   width: calc(100% - 250px);
   border: 1px solid red;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  box-sizing: border-box;
+`;
+
+const TitleBox = styled.div`
+  width: 100%;
+  height: 20%;
+  border: 1px solid red;
+  padding: 20px;
+  box-sizing: border-box;
+`;
+
+const ContentsBox = styled.div`
+  width: 100%;
+  height: 80%;
+  border: 1px solid black;
+  padding: 20px;
+  box-sizing: border-box;
 `;
 
 function AllQuestions() {
   return (
     <Box>
-      <h2>AllQuestions</h2>
+      <TitleBox>
+        <h2>안녕</h2>
+      </TitleBox>
+      <ContentsBox>
+        <h2>질문 목록</h2>
+      </ContentsBox>
     </Box>
   );
 }
