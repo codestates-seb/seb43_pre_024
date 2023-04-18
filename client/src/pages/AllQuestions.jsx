c32f99e2d1f554fa6ecf83663e25a8507b99c9b5
jieuny0314 <ccomo0313@naver.com>
2023-04-18 21:25:20 +0900

    Feat : 더미데이터 5개 받아오기 및 질문 목록 디자인

--- client/src/pages/AllQuestions.jsx
+++ client/src/pages/AllQuestions.jsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { useState } from 'react';
+import { GoCheck } from 'react-icons/go';
 import useFetch from '../util/useFetch';
 
 const Box = styled.div`
@@ -16,7 +17,7 @@ const TitleBox = styled.div`
   width: 80%;
   height: 20%;
   border-bottom: 1px solid rgb(214, 216, 219);
-  padding: 45px 50px 20px 30px;
+  padding: 45px 0px 20px 30px;
   box-sizing: border-box;
 
   .firstLine,
@@ -119,42 +120,140 @@ const TopBtn = styled.button`
 `;
 
 const ContentsBox = styled.div`
-  width: 100%;
+  width: 80%;
   height: 100%;
-  border: 1px solid green;
-  padding: 20px;
   box-sizing: border-box;
 `;
 
 const QuestionBox = styled.div`
-  border: 1px solid red;
+  border-bottom: 1px solid rgb(214, 216, 219);
   width: 100%;
   height: 200px;
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
-  border: 1px solid red;
   box-sizing: border-box;
+  margin-bottom: 20px;
 `;
 
 const CountBox = styled.div`
   width: 20%;
-  border: 1px solid blue;
+  height: 150px;
   display: flex;
   flex-direction: column;
+  justify-content: center;
+  align-items: end;
+  padding-right: 30px;
   box-sizing: border-box;
+
+  .likesValue {
+    font-weight: bold;
+    margin-right: 5px;
+    font-size: 1.5rem;
+  }
+
+  .likesText {
+    font-size: 1.3rem;
+  }
+
+  .answersValue {
+    font-weight: bold;
+    margin-right: 5px;
+    font-size: 1.5rem;
+  }
+
+  .answersText {
+    font-size: 1.3rem;
+  }
+
+  .views {
+    margin-top: 5px;
+    margin-bottom: 5px;
+  }
+
+  .viewsValue {
+    font-weight: bold;
+    margin-right: 5px;
+    font-size: 1.5rem;
+    color: rgb(153, 88, 90);
+  }
+
+  .viewsText {
+    font-size: 1.3rem;
+    color: rgb(153, 88, 90);
+  }
+`;
+
+const Answers = styled.div`
+  background-color: ${props =>
+    props.accepted === true ? 'rgb(74, 111, 72)' : 'white'};
+  color: ${props => (props.accepted === true ? 'white' : 'rgb(74, 111, 72)')};
+  border: ${props =>
+    props.accepted === true ? 'none' : '1px solid rgb(74, 111, 72)'};
+  padding: 5px;
+  padding-right: 10px;
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+  justify-content: end;
+  width: 140px;
+
+  .answersValue {
+    margin-left: 10px;
+  }
 `;
 
 const QuestionMain = styled.div`
   width: 80%;
-  border: 1px solid green;
+  height: 150px;
   box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+
+  .title {
+    color: rgb(57, 116, 194);
+    font-size: 1.7rem;
+    margin-bottom: 5px;
+
+    :hover {
+      color: rgb(77, 139, 221);
+    }
+  }
+
+  .content {
+    font-size: 1.3rem;
+    line-height: 1.7rem;
+    display: block;
+    width: 100%;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: normal;
+    line-height: 1.2;
+    text-align: left;
+    word-wrap: break-word;
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+  }
 `;
 
 const QuestionProfile = styled.div`
   width: 100%;
-  border: 1px solid black;
+  height: 50px;
   box-sizing: border-box;
+  text-align: right;
+
+  .name {
+    margin-right: 10px;
+    font-size: 1.1rem;
+    color: rgb(77, 139, 221);
+  }
+
+  .createdAt {
+    font-size: 1.1rem;
+    color: rgb(91, 92, 93);
+  }
 `;
 
 function AllQuestions() {
@@ -235,22 +334,26 @@ function AllQuestions() {
                       <span className="likesValue">{data.likes}</span>
                       <span className="likesText">likes</span>
                     </div>
-                    <div className="answers">
+                    <Answers accepted={data.accepted}>
+                      {data.accepted === true ? <GoCheck size="30" /> : null}
                       <span className="answersValue">{data.answer_cnt}</span>
                       <span className="answersText">answers</span>
-                    </div>
+                    </Answers>
                     <div className="views">
                       <span className="viewsValue">{data.views}</span>
                       <span className="viewsText">views</span>
                     </div>
                   </CountBox>
                   <QuestionMain>
-                    {data.title}
-                    {data.content}
+                    <span className="title">{data.title}</span>
+                    <span className="content">{data.content}</span>
                   </QuestionMain>
                   <QuestionProfile>
-                    {data.name}
-                    {data.createdAt}
+                    <span className="name">{data.name}</span>
+                    <span className="createdAt">
+                      asked {data.created_at.slice(0, 10)} &nbsp;at
+                      {data.created_at.slice(10)}
+                    </span>
                   </QuestionProfile>
                 </QuestionBox>
               );
