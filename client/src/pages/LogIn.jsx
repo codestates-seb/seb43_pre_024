614bc31c64a390848e6cda3d990abc6cd5bb46a5
cheesepizza453 <earlykim1209@gmail.com>
2023-04-21 11:32:17 +0900

    Design: 깃 구글 로그인 버튼 추가 및 svg이미지 파일 추가

--- client/src/pages/LogIn.jsx
+++ client/src/pages/LogIn.jsx
@@ -1,6 +1,9 @@
 /* eslint-disable consistent-return */
 import styled from 'styled-components';
 import { useState, useEffect } from 'react';
+import { ReactComponent as GoogleLogo } from '../images/googleLogo.svg';
+import { ReactComponent as GitLogo } from '../images/gitLogo.svg';
+import { ReactComponent as StackLogo } from '../images/stackoverflowMiniLogo.svg';
 
 import { validateEmail, validatePassword } from '../util/validator';
 
@@ -14,7 +17,7 @@ const LoginBox = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    margin-top: 20vh;
+    margin-top: 170px;
     width: 290px;
   }
 `;
@@ -23,7 +26,34 @@ const LoginLogo = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  margin-bottom: 20px;
+  margin-bottom: 30px;
+`;
+
+const GoogleLogin = styled.button`
+width: 100%;
+padding 8px 0;
+display: flex;
+align-items: center;
+justify-content: center;
+background:#fff;
+color:#2f3337;
+border: 1px solid #ccc;
+border-radius: 5px;
+
+svg{margin-right: 5px;}
+`;
+const GitLogin = styled.button`
+  width: 100%;
+  padding 8px 0;
+  margin: 10px 0 30px 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background:#2f3337;
+  color:#fff;
+  border-radius: 5px;
+
+  svg{margin-right: 5px;}
 `;
 
 const InputBox = styled.div`
@@ -165,14 +195,16 @@ function LogIn() {
     <LoginBox>
       <div className="container">
         <LoginLogo>
-          <svg aria-hidden="true" width="32" height="37" viewBox="0 0 32 37">
-            <path d="M26 33v-9h4v13H0V24h4v9h22Z" fill="#BCBBBB" />
-            <path
-              d="m21.5 0-2.7 2 9.9 13.3 2.7-2L21.5 0ZM26 18.4 13.3 7.8l2.1-2.5 12.7 10.6-2.1 2.5ZM9.1 15.2l15 7 1.4-3-15-7-1.4 3Zm14 10.79.68-2.95-16.1-3.35L7 23l16.1 2.99ZM23 30H7v-3h16v3Z"
-              fill="#F48024"
-            />
-          </svg>
+          <StackLogo />
         </LoginLogo>
+        <GoogleLogin>
+          <GoogleLogo />
+          Log in with Google
+        </GoogleLogin>
+        <GitLogin>
+          <GitLogo />
+          Log in with GitHub
+        </GitLogin>
         <InputBox>
           <div className="inputArea">
             <label htmlFor="email">
