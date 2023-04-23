7a510da41120ad5a100dcb1961f8c813d73b4a47
dakyungoh <dakyung.work@gmail.com>
2023-04-23 21:33:36 +0900

    Feat : 좋아요 및 채택 UI구현

--- client/src/components/AnswerList.jsx
+++ client/src/components/AnswerList.jsx
@@ -15,7 +15,7 @@ function AnswerList({ questionId }) {
       AnswerId: 14,
       title: "답변입니다",
       content:
-        "\n good~~  hihi~~\n ```javascript\n const a = 235242; const b = 7654345\n console.log(a);\nconsole.log(b)\n```",
+        "```javascript\n const a = 235242; const b = 7654345\n console.log(a);\nconsole.log(b)\n```",
       name: "김미리",
       likes: "140",
       answer_cnt: "8",
@@ -23,31 +23,69 @@ function AnswerList({ questionId }) {
       created_at: "2023-04-18 18:52",
       accepted_answer: true,
     },
+    {
+      AnswerId: 14,
+      title: "저도 답변 하나 추가요",
+      content:
+        "good~~  hihi~~\n ```javascript\n const a = 123124; \nconst b = 43252314\n console.log(a);\nconsole.log(b)\n```",
+      name: "오다경",
+      likes: "183",
+      answer_cnt: "8",
+      views: "74",
+      created_at: "2023-04-18 18:52",
+      accepted_answer: true,
+    },
   ];
 
   const [answerCount, setAnswerCount] = useState(data.length);
-  const [count, setCount] = useState(0);
 
   const onClickHeartButton = () => {
-    setCount(count + 1);
+    // TODO: 좋아요 API 연동
   };
+
   return (
-    <div>
+    <div style={{ width: "80%" }}>
       <div style={{ fontSize: "20px", margin: 0 }}>{answerCount} Answer</div>
       {data.map((answer) => (
-        <div key={answer.AnswerId}>
-          <div>{answer.title}</div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "rows",
+            gap: 20,
+            marginTop: 30,
+            marginBottom: 30,
+            paddingBottom: 30,
+            borderBottom: "1px solid #eee",
+          }}
+        >
+          <div style={{ margin: "auto" }}>
+            <HeartButtonStyle>
+              <AiFillHeart
+                size="20"
+                style={{ color: "red" }}
+                onClick={() => onClickHeartButton()}
+              />
+              {answer.likes}
+            </HeartButtonStyle>
+            <div>
+              <button
+                onclick={() => {
+                  // TODO: 채택하기 API 연동
+                  // if(isLogin && isAuthor) {
+                  // }
+                }}
+              >
+                채택하기
+              </button>
+            </div>
+          </div>
+
+          <div key={answer.AnswerId} style={{ flex: 1 }}>
+            <div style={{ marginBottom: 20 }}>{answer.title}</div>
+            <MDEditor.Markdown source={answer.content} />
+          </div>
         </div>
       ))}
-      <HeartButtonStyle>
-        <AiFillHeart
-          size="20"
-          style={{ color: "red" }}
-          onClick={() => onClickHeartButton()}
-        />
-        {count}
-      </HeartButtonStyle>
-      <MDEditor.Markdown source={data[0].content} style={{ width: "80%" }} />
     </div>
   );
 }
